fa8aa412b0327061f8df68a30f7c63163da013ff
sbcdwl <2191678009@qq.com>
2026-06-05 15:29:18 +0800

    补充代码注释

--- CppChat/Server/Server.cpp
+++ CppChat/Server/Server.cpp
@@ -33,8 +33,10 @@ Server::Server(const std::string& host, uint16_t port)
 	addr.sin_family = AF_INET;	
 	addr.sin_port = htons(port);		//大端和小端存储，本机都是小端存储，网络上传输的为大端
 										//|1|2|,由低到高为大端存储,htons将小端转换为大端(host to net short)
-	inet_pton(AF_INET, host.data(), &addr.sin_addr);//IP地址
-
+	inet_pton(AF_INET, host.data(), &addr.sin_addr);//把“地址：端口”转换为下面bind用的二进制，并传入addr.sin_addr
+	//至此sockaddr_in的三个参数完成
+	
+	//bind把socket和一个“地址：端口”绑定，
 	if (bind(m_fd, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
 	{
 		std::cerr << "bind error" << WSAGetLastError() << std::endl;
